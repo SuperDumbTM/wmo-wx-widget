7eddebc821c94ff1b66a984ddcf52c5809d10637
SuperDumbTM <eltonlochunkit@gmail.com>
2024-06-21 21:01:55 +0800

    Port complete

---
+++ src/app/forecast/[id]/components/vertical_layout.tsx
@@ -0,0 +1,118 @@
+import Image from "next/image";
+
+import {FutureWeather, PresentWeather} from "@/libs/wmo/definition";
+import {Locale} from "@/libs/wmo/enums";
+
+import * as wmo from "../../../../libs/wmo/wmo";
+
+export default function VerticalLayout({
+  locale,
+  weather,
+  forecast,
+}: {
+  locale: Locale;
+  weather: PresentWeather;
+  forecast: FutureWeather;
+}) {
+  function renderForecasts() {
+    if (forecast.forecasts.length == 0) {
+      return (
+        <div className="flex flex-row justify-center items-center m-1 h-20 rounded-border">
+          <span className="text-red-600">Forecast Not Available</span>
+        </div>
+      );
+    }
+
+    return forecast.forecasts.map(function (wx, idx) {
+      let _d = new Date(wx.date);
+
+      return (
+        <div className="flex flex-row m-1 rounded-border" key={idx}>
+          <div
+            className="flex flex-col justify-center items-center"
+            style={{minWidth: "30%"}}
+          >
+            <span className="text-gray-400" style={{fontSize: "0.57rem"}}>
+              {_d.toLocaleString(wmo.wmoToIso639(locale), {weekday: "long"})}{" "}
+            </span>
+            <span>{_d.getDate()}</span>
+          </div>
+
+          <div className="flex grow justify-center items-center">
+            <Image
+              src={wx.icon!}
+              width={60}
+              height={40}
+              alt="Weather Icon"
+            ></Image>
+          </div>
+
+          <div
+            className="flex flex-col justify-center items-center"
+            style={{minWidth: "30%"}}
+          >
+            <span className="text-sky-600 mx-1">
+              <i className="bi bi-thermometer-low"></i>
+              {`${wx.temp.min}${wx.temp.unit}`}
+            </span>
+            <span className="text-red-600 mx-1">
+              <i className="bi bi-thermometer-high"></i>
+              {`${wx.temp.max}${wx.temp.unit}`}
+            </span>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <div className="flex flex-col flex-nowrap">
+      <div className="flex flex-col items-center m-1 rounded-border">
+        <div className="flex my-1">
+          <span className="text-gray-600">
+            <i className="bi bi-geo"></i>
+            <span className="me-1">{weather.city.name}</span>
+          </span>
+        </div>
+
+        <div className="flex justify-around items-center my-1">
+          <Image
+            src={weather.icon!}
+            width={60}
+            height={40}
+            alt="Weather Icon"
+          ></Image>
+
+          <span className="fw-bold fs-4">
+            {`${weather.temp}${weather.tempUnit}`}
+          </span>
+
+          <div className="flex mx-2" style={{height: "2.5rem;"}}>
+            <div className="vr"></div>
+          </div>
+
+          <div className="flex flex-col">
+            <div className="flex my-1">
+              <span className="text-muted">
+                <i className="bi bi-droplet-half"></i>
+                {`${weather.rh || "--"}%`}
+              </span>
+            </div>
+            <div className="flex my-1">
+              <span className="text-muted">
+                <i className="bi bi-wind"></i>{" "}
+                {weather.wind
+                  ? `${weather.wind.direction} ${
+                      weather.wind.speed || "--"
+                    } m/s`
+                  : "--"}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {renderForecasts()}
+    </div>
+  );
+}
--- src/app/forecast/[id]/page.tsx
+++ src/app/forecast/[id]/page.tsx
@@ -3,7 +3,8 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 import * as wmo from "../../../libs/wmo/wmo";
 import {Locale, TempUnit} from "@/libs/wmo/enums";
-import HorizontalForecast from "./components/horiontal_forecast";
+import HorizontalLayout from "./components/horizontal_layout";
+import VerticalLayout from "./components/vertical_layout";
 
 export default async function Page({
   params,
@@ -19,75 +20,16 @@ export default async function Page({
     wmo.forecasts(params.id, locale, unit),
   ]);
 
-  function getWindSpeed() {
-    if (pwx.wind) {
-      return `${pwx.wind.direction} ${pwx.wind.speed || "--"} m/s`;
-    } else {
-      return "--";
-    }
-  }
-
   return (
     <div
       className={`flex items-${searchParams?.align || "start"} min-h-screen`}
     >
       <div className="hidden md:block w-full">
-        <div
-          className="flex flex-row justify-around items-center m-1"
-          style={{border: "1px lightgray solid", borderRadius: "5px"}}
-        >
-          <div className="flex mx-1 lg:mx-0">
-            <div className="flex flex-col items-center">
-              <div className="flex">
-                <span className="text-secondary">
-                  <i className="bi bi-geo"></i>
-                  <span className="me-1">{pwx.city.name}</span>
-                </span>
-              </div>
-
-              <div className="flex items-center my-1">
-                <Image
-                  src={pwx.icon!}
-                  width={70}
-                  height={50}
-                  alt="Weather Icon"
-                ></Image>
-                {/* <Image className="wx-icon" src="<%= pwx.icon %>"></Image> */}
-                <span className="fw-bold fs-4">
-                  {`${pwx.temp || "--"}${pwx.tempUnit}`}
-                </span>
-              </div>
-
-              <div className="flex flex-col lg:flex-row text-muted">
-                <span className="mx-1">
-                  <i className="bi bi-droplet-half"></i>
-                  {`${pwx.rh || "--"}%`}
-                </span>
-                <span className="mx-1">
-                  <i className="bi bi-wind"></i> {getWindSpeed()}
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="d-flex mx-2" style={{height: "2.5rem"}}>
-            <div className="inline-block h-[70px] min-h-[1em] w-px bg-black"></div>
-          </div>
-
-          <HorizontalForecast locale={locale} forecast={wx} />
-          {/* {
-            if (forecasts.length == 0) {
-                <div className="d-flex flex-row justify-content-center align-items-center my-1 round-border" style="height: 5rem;">
-                    <span className="text-danger">
-                        Forecast Not Available
-                    </span>
-                </div>
-              }
-            }
-          } */}
-        </div>
+        <HorizontalLayout locale={locale} weather={pwx} forecast={wx} />
       </div>
-      <div className="block md:hidden w-full"></div>
+      <div className="block md:hidden w-full">
+        <VerticalLayout locale={locale} weather={pwx} forecast={wx} />
+      </div>
     </div>
   );
 }
