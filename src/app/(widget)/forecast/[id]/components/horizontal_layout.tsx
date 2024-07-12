2cdfd5b139e7b013bc2dd170c32afd9e88efb100
SuperDumbTM <eltonlochunkit@gmail.com>
2024-07-12 15:53:16 +0800

    Reposition weather descr

--- src/app/(widget)/forecast/[id]/components/horizontal_layout.tsx
+++ src/app/(widget)/forecast/[id]/components/horizontal_layout.tsx
@@ -29,13 +29,24 @@ async function CurrentWeather({weather}: {weather: PresentWeather}) {
           <span className="font-bold text-2xl">
             {`${weather.temp || "--"}${weather.tempUnit}`}
           </span>
-          <span className="text-xs">
+        </div>
+        {/* <div className="flex flex-col items-center my-1 max-w-32">
+          <span className="font-bold text-2xl">
+            {`${weather.temp || "--"}${weather.tempUnit}`}
+          </span>
+          <span className="w-full text-xs truncate">
             {weather.weather ? t(weather.weather) : ""}
           </span>
-        </div>
+        </div> */}
       </div>
 
-      <div className="flex flex-row text-muted">
+      <div className="flex my-1 max-w-50">
+        <span className="w-full text-xs truncate">
+          {weather.weather ? t(weather.weather) : ""}
+        </span>
+      </div>
+
+      <div className="flex flex-row">
         <span className="mx-1">
           <i className="bi bi-droplet-half"></i> {`${weather.rh || "--"}%`}
         </span>
