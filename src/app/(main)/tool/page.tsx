2210b4181bba7f00cc4fd0a9867445d5f6d0b8ef
SuperDumbTM <eltonlochunkit@gmail.com>
2024-08-19 12:22:50 +0800

    Tidying styles

--- src/app/(main)/tool/page.tsx
+++ src/app/(main)/tool/page.tsx
@@ -41,121 +41,117 @@ export default function Page() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex flex-col p-8 items-center justify-center">
       <form>
-        <div className="space-y-4">
-          <div className="border-b border-gray-900/10 pb-6">
-            <h1 className="text-xl font-semibold leading-7 text-gray-900">
-              {t("widgetCustomiser")}
-            </h1>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
+        <div className="space-y-6">
+          <div>
+            <h1 className="text-3xl font-bold">{t("widgetCustomiser")}</h1>
+            <p className="mt-1 text-sm text-gray-400">
               {t("widgetCustomiserHelp")}
             </p>
           </div>
 
-          <div className="border-b border-gray-900/10 pb-6">
-            <div className="flex flex-wrap -mx-3 mb-3">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  htmlFor="locale"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  {t("language")}
-                </label>
-                <div className="mt-2">
-                  <select
-                    name="locale"
-                    defaultValue={language}
-                    onChange={(e) => {
-                      setFormData({...formData, locale: e.target.value});
-                      setLanguage(e.target.value);
+          <div className="border-b border-gray-900/10 pb-4">
+            <h2 className="text-xl font-bold text-gray-900"></h2>
+            <p className="mt-1 text-sm text-gray-400"></p>
 
-                      if (e.target.value.length != 2) {
-                        setCityOption([]);
-                        return;
-                      }
+            <div className="mt-6 space-y-4">
+              <div className="flex flex-wrap -mx-3 space-y-4 md:space-y-0">
+                <div className="w-full md:w-1/2 px-3">
+                  <label htmlFor="locale" className="block text-base">
+                    {t("language")}
+                  </label>
+                  <div className="mt-2">
+                    <select
+                      name="locale"
+                      defaultValue={language}
+                      onChange={(e) => {
+                        setFormData({...formData, locale: e.target.value});
+                        setLanguage(e.target.value);
 
-                      getCity(
-                        Locale[
-                          (e.target.value[0].toUpperCase() +
-                            e.target.value.slice(1)) as keyof typeof Locale
-                        ],
-                      )
-                        .then((cities) => {
-                          setCityOption(
-                            cities.map((v, i) => {
-                              return {value: v.id.toString(), label: v.name};
-                            }),
-                          );
-                        })
-                        .catch((_) => []);
-                    }}
-                    autoComplete="country-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                  >
-                    <option value={undefined}>-----</option>
-                    <option value="ar">لعربية</option>
-                    <option value="en">English</option>
-                    <option value="tc">繁體中文</option>
-                    <option value="zh">简体中文</option>
-                    <option value="fr">Français</option>
-                    <option value="de">Deutsch</option>
-                    <option value="it">Italiano</option>
-                    <option value="kr">한국어</option>
-                    <option value="pl">Polski</option>
-                    <option value="pt">Português</option>
-                    <option value="ru">Русский</option>
-                    <option value="es">Español</option>
-                  </select>
+                        if (e.target.value.length != 2) {
+                          setCityOption([]);
+                          return;
+                        }
+
+                        getCity(
+                          Locale[
+                            (e.target.value[0].toUpperCase() +
+                              e.target.value.slice(1)) as keyof typeof Locale
+                          ],
+                        )
+                          .then((cities) => {
+                            setCityOption(
+                              cities.map((v, i) => {
+                                return {value: v.id.toString(), label: v.name};
+                              }),
+                            );
+                          })
+                          .catch((_) => []);
+                      }}
+                      autoComplete="country-name"
+                      className="block w-full rounded-[4px] border-0 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm"
+                      style={{height: "38px"}}
+                    >
+                      <option value={undefined}>-----</option>
+                      <option value="ar">لعربية</option>
+                      <option value="en">English</option>
+                      <option value="tc">繁體中文</option>
+                      <option value="zh">简体中文</option>
+                      <option value="fr">Français</option>
+                      <option value="de">Deutsch</option>
+                      <option value="it">Italiano</option>
+                      <option value="kr">한국어</option>
+                      <option value="pl">Polski</option>
+                      <option value="pt">Português</option>
+                      <option value="ru">Русский</option>
+                      <option value="es">Español</option>
+                    </select>
+                  </div>
                 </div>
-              </div>
 
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  htmlFor="country"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  {t("city")}
-                </label>
-                <div className="mt-2">
-                  <AsyncSelect
-                    instanceId="city"
-                    name="city"
-                    defaultOptions={cityOption}
-                    loadOptions={(inputValue, callback) => {
-                      callback(
-                        cityOption.filter((c) =>
-                          c.label
-                            .toLocaleLowerCase()
-                            .includes(inputValue.toLocaleLowerCase()),
-                        ),
-                      );
-                    }}
-                    onChange={(newValue) => {
-                      setFormData({...formData, city: newValue!.value});
-                    }}
-                    filterOption={createFilter({ignoreAccents: false})}
-                    className="block w-full sm:max-w-xs sm:text-sm sm:leading-6"
-                  ></AsyncSelect>
+                <div className="w-full md:w-1/2 px-3">
+                  <label htmlFor="country" className="block text-base">
+                    {t("city")}
+                  </label>
+                  <div className="mt-2">
+                    <AsyncSelect
+                      instanceId="city"
+                      name="city"
+                      defaultOptions={cityOption}
+                      loadOptions={(inputValue, callback) => {
+                        callback(
+                          cityOption.filter((c) =>
+                            c.label
+                              .toLocaleLowerCase()
+                              .includes(inputValue.toLocaleLowerCase()),
+                          ),
+                        );
+                      }}
+                      onChange={(newValue) => {
+                        setFormData({...formData, city: newValue!.value});
+                      }}
+                      filterOption={createFilter({ignoreAccents: false})}
+                      className="block w-full sm:max-w-xs sm:text-sm"
+                    ></AsyncSelect>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="border-b border-gray-900/10 pb-6">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">
+          <div className="border-b border-gray-900/10 pb-4">
+            <h2 className="text-xl font-bold text-gray-900">
               {t("displaySettings")}
             </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600"></p>
+            <p className="mt-1 text-sm text-gray-400"></p>
 
-            <div className="mt-10 space-y-10">
+            <div className="mt-6 space-y-4">
               <fieldset>
-                <legend className="text-sm font-semibold leading-6 text-gray-900">
-                  {t("temperatureUnit")}
-                </legend>
-                <p className="mt-1 text-sm leading-6 text-gray-600"></p>
+                <legend className="text-base">{t("temperatureUnit")}</legend>
+                <p className="mt-1 text-sm text-gray-500"></p>
 
-                <div className="flex mt-6 ">
+                <div className="flex mt-2">
                   <div className="flex items-center me-4">
                     <input
                       type="radio"
@@ -197,12 +193,10 @@ export default function Page() {
               </fieldset>
 
               <fieldset>
-                <legend className="text-sm font-semibold leading-6 text-gray-900">
-                  {t("widgetAlignment")}
-                </legend>
-                <p className="mt-1 text-sm leading-6 text-gray-600"></p>
+                <legend className="text-base">{t("widgetAlignment")}</legend>
+                <p className="mt-1 text-sm text-gray-500"></p>
 
-                <div className="flex mt-6 ">
+                <div className="flex mt-2">
                   <div className="flex items-center me-4">
                     <input
                       type="radio"
@@ -263,14 +257,12 @@ export default function Page() {
               </fieldset>
 
               <fieldset>
-                <legend className="text-sm font-semibold leading-6 text-gray-900">
-                  {t("maxForecastPeriod")}
-                </legend>
-                <p className="mt-1 text-sm leading-6 text-gray-600">
+                <legend className="text-base">{t("maxForecastPeriod")}</legend>
+                <p className="mt-1 text-xs text-gray-400">
                   {t("maxForecastPeriodHelp")}
                 </p>
 
-                <div className="flex mt-6 ">
+                <div className="flex mt-2">
                   <div className="flex items-center me-4">
                     <button
                       type="button"
@@ -323,95 +315,101 @@ export default function Page() {
             </div>
           </div>
 
-          <div className="flex flex-col space-y-2">
-            <label className="text-lg font-medium text-gray-700">URL</label>
-            <div className="flex items-center space-x-2">
-              <div className="relative flex-1">
-                <input
-                  type="text"
-                  value={outUrl}
-                  className="w-full px-3 py-3 pr-10 text-sm border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                  readOnly
-                  disabled
-                />
-                <button
-                  type="button"
-                  className="absolute inset-y-0 right-0 px-4 rounded-lg text-gray-500 hover:bg-gray-400/20"
-                  onClick={(e) => {
-                    if (!copied) {
-                      setTimeout(() => {
-                        setCopied(false);
-                      }, 2000);
-                    }
-                    setCopied(true);
+          <div>
+            <h2 className="text-xl font-bold text-gray-900">URL</h2>
+            <p className="mt-1 text-sm text-gray-400"></p>
 
-                    navigator.clipboard.writeText(outUrl);
-                  }}
-                >
-                  <span className={copied ? "hidden" : ""}>
-                    <svg
-                      className="w-3.5 h-3.5"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 18 20"
+            <div className="mt-6 space-y-4">
+              <div className="flex flex-col space-y-2">
+                <div className="flex items-center space-x-2">
+                  <div className="relative flex-1">
+                    <input
+                      type="text"
+                      value={outUrl}
+                      className="w-full px-3 py-3 pr-10 text-sm border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                      readOnly
+                      disabled
+                    />
+                    <button
+                      type="button"
+                      className="absolute inset-y-0 right-0 px-4 rounded-lg text-gray-500 hover:bg-gray-400/20"
+                      onClick={(e) => {
+                        if (!copied) {
+                          setTimeout(() => {
+                            setCopied(false);
+                          }, 2000);
+                        }
+                        setCopied(true);
+
+                        navigator.clipboard.writeText(outUrl);
+                      }}
                     >
-                      <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
-                    </svg>
-                  </span>
-                  <span
-                    className={`${
-                      copied ? "" : "hidden"
-                    } inline-flex items-center`}
+                      <span className={copied ? "hidden" : ""}>
+                        <svg
+                          className="w-3.5 h-3.5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 18 20"
+                        >
+                          <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                        </svg>
+                      </span>
+                      <span
+                        className={`${
+                          copied ? "" : "hidden"
+                        } inline-flex items-center`}
+                      >
+                        <svg
+                          className="w-3.5 h-3.5 text-green-700"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 16 12"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 5.917 5.724 10.5 15 1.5"
+                          />
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+                  <button
+                    type="button"
+                    className="px-4 py-3 text-white rounded-lg bg-indigo-600 hover:bg-indigo-500 focus:outline-none"
+                    onClick={(_) => {
+                      if (formData.city == "") {
+                        setOutUrl("");
+                        return;
+                      }
+
+                      setOutUrl(
+                        `${location.host}/forecast/${
+                          formData.city
+                        }?${new URLSearchParams(
+                          Object.fromEntries(
+                            Object.entries(formData).filter(([k, v]) => {
+                              return v != null && v != "" && k != "city";
+                            }),
+                          ),
+                        )}`,
+                      );
+                    }}
                   >
-                    <svg
-                      className="w-3.5 h-3.5 text-green-700"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 16 12"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5.917 5.724 10.5 15 1.5"
-                      />
-                    </svg>
-                  </span>
-                </button>
+                    {t("generate")}
+                  </button>
+                </div>
+                <p
+                  className={`${
+                    formData.city == "" ? "" : "hidden"
+                  } text-red-500 text-xs italic`}
+                >
+                  {t("emptyCityValidation")}
+                </p>
               </div>
-              <button
-                type="button"
-                className="px-4 py-3 text-white rounded-lg bg-indigo-600 hover:bg-indigo-500 focus:outline-none"
-                onClick={(_) => {
-                  if (formData.city == "") {
-                    setOutUrl("");
-                    return;
-                  }
-
-                  setOutUrl(
-                    `${location.host}/forecast/${
-                      formData.city
-                    }?${new URLSearchParams(
-                      Object.fromEntries(
-                        Object.entries(formData).filter(([k, v]) => {
-                          return v != null && v != "" && k != "city";
-                        }),
-                      ),
-                    )}`,
-                  );
-                }}
-              >
-                {t("generate")}
-              </button>
             </div>
-            <p
-              className={`${
-                formData.city == "" ? "" : "hidden"
-              } text-red-500 text-xs italic`}
-            >
-              {t("emptyCityValidation")}
-            </p>
           </div>
         </div>
       </form>
